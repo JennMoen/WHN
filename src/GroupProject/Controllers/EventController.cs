77294c161b9ee1ac378b1e0228caa1dd4d17845a
Vickey Crouch <crouchvickey@gmail.com>
2016-08-05 10:58:51 -0500

    Added repository, DTO, Service, and Controller for the Event table.  Added the GetAllEvents method to the Service file.

---
+++ src/GroupProject/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GroupProject.Data;
+using GroupProject.Services;
+
+namespace GroupProject.Controllers
+{
+    [Route ("api/[controller]")]
+
+    public class EventController : Controller
+    {
+        private EventService _eventService;
+        public EventController(EventService es) {
+            _eventService = es;
+        }
+
+        [HttpGet]
+        public IList<EventDTO>GetAllEvents() {
+            return _eventService.GetAllEvents();
+
+        }
+    }
+}
---
+++ src/GroupProject/Infrastructure/EventRepository.cs
@@ -0,0 +1,22 @@
+﻿using GroupProject.Data;
+using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Infrastructure
+{
+    public class EventRepository {
+        private ApplicationDbContext _db;
+        public EventRepository(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        //get all events
+        public IQueryable<Event> GetAllEvents() {
+            return _db.Events;
+        }
+    }
+
+}
