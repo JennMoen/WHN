243ca3822b798cc980eeb334322f829356e1a782
Vickey Crouch <crouchvickey@gmail.com>
2016-08-05 15:17:17 -0500

    added repository, Service, DTO, and Controller for the Groups and Feedback tables

---
+++ src/GroupProject/Data/FeedbackDTO.cs
@@ -0,0 +1,22 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Data
+{
+    public class FeedbackDTO
+    {
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+    }
+}
